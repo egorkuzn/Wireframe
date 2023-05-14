df017589c968d8740989a297d8717044dbf0500f
shift-lab-kuznetsov <e.kuznetsov1@g.nsu.ru>
2023-05-14 23:27:05 +0700

    Поменял цветовую гамму. Реализован перевод из экранных координат в координаты сплайна и обратно

--- src/main/java/ru/nsu/fit/g20204/kuznetsov/wireframe/b_spline_editor/BSplinePane.java
+++ src/main/java/ru/nsu/fit/g20204/kuznetsov/wireframe/b_spline_editor/BSplinePane.java
@@ -1,5 +1,7 @@
 package ru.nsu.fit.g20204.kuznetsov.wireframe.b_spline_editor;
 
+import ru.nsu.fit.g20204.kuznetsov.wireframe.math.BSpline;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
@@ -20,13 +22,13 @@ public class BSplinePane extends JPanel implements MouseWheelListener, MouseMoti
         this.addMouseMotionListener(this);
     }
 
-    private Color backgroundColor = new Color(45, 45, 51);
-    private Color xAxisColor = new Color(255, 255, 255);
-    private Color yAxisColor = new Color(255, 255, 255);
-    private Color keyPointColor = new Color(0, 0, 0);
-    private Color selectedKeyPointColor = new Color(220, 12, 12);
-    private Color brokenLineColor = new Color(255, 255, 0);
-    private Color splineColor = new Color(77, 255, 0);
+    private Color backgroundColor = new Color(169, 169, 169);
+    private Color xAxisColor = new Color(51, 51, 51);
+    private Color yAxisColor = new Color(51, 51, 51);
+    private Color keyPointColor = new Color(42, 122, 255);
+    private Color selectedKeyPointColor = new Color(255, 0, 0);
+    private Color brokenLineColor = new Color(42, 46, 54);
+    private Color splineColor = new Color(94, 255, 0);
 
     private int axisSize = 2;
     private int splineSize = 2;
@@ -97,6 +99,83 @@ public class BSplinePane extends JPanel implements MouseWheelListener, MouseMoti
     }
     
     private void paintSpline(Graphics2D g2d) {
-        if (spline.getKeyPoints().isEmpty())
+        if (spline.getKeyPoints().isEmpty()) {
+            return;
+        }
+
+        connectPointsWithStraightLines(g2d);
+        showKeyPoints(g2d);
+        showSpline(g2d);
+    }
+
+    private void connectPointsWithStraightLines(Graphics2D g2d) {
+        g2d.setStroke(new BasicStroke(brokenLineSize));
+        g2d.setColor(brokenLineColor);
+        Point previousScreenKeyPoint = getPointOnScreen(spline.getKeyPoints().get(0));
+
+        for (var point: spline.getKeyPoints()) {
+            Point screenPoint = getPointOnScreen(point);
+
+            g2d.drawLine(previousScreenKeyPoint.x, previousScreenKeyPoint.y, screenPoint.x, screenPoint.y);
+
+            previousScreenKeyPoint = screenPoint;
+        }
+    }
+
+    private Point getPointOnScreen(Point2D.Double point) {
+        return getPointOnScreen(point.getX(), point.getY());
+    }
+
+    private Point getPointOnScreen(double x, double y) {
+        int xPixelDistance = (int) (x * pixelsPerIndentStep);
+        int yPixelDistance = (int) (-y * pixelsPerIndentStep);
+
+        int xAbsolutePos = xPixelDistance + (this.getWidth() / 2 + horizontalOffset);
+        int yAbsolutePos = yPixelDistance + (this.getHeight() / 2 + verticalOffset);
+
+        return new Point(xAbsolutePos, yAbsolutePos);
+    }
+
+    private Point2D.Double getContinuousPoint(int x, int y) {
+        int xPixelDistance = x - (this.getWidth() / 2 + horizontalOffset);
+        int yPixelDistance = y - (this.getHeight() / 2 + verticalOffset);
+
+        double xContinuous = (double) xPixelDistance / pixelsPerIndentStep;
+        double yContinuous = (double) yPixelDistance / pixelsPerIndentStep;
+
+        return new Point2D.Double(xContinuous, -yContinuous);
+    }
+
+    private void showKeyPoints(Graphics2D g2d) {
+        g2d.setColor(keyPointColor);
+
+        for (int keyPointIndex = 0; keyPointIndex < spline.getKeyPoints().size(); keyPointIndex++) {
+            Point2D.Double keyPoint = spline.getKeyPoints().get(keyPointIndex);
+
+            Point screenPoint = this.getPointOnScreen(keyPoint.x, keyPoint.y);
+
+            if (keyPointIndex == dragPointIndex)
+                g2d.setColor(selectedKeyPointColor);
+            else
+                g2d.setColor(keyPointColor);
+            g2d.drawOval(screenPoint.x - pointRadius, screenPoint.y - pointRadius, pointRadius * 2, pointRadius * 2);
+        }
+    }
+
+    private void showSpline(Graphics2D g2d) {
+        g2d.setStroke(new BasicStroke(splineSize));
+        g2d.setColor(splineColor);
+        var splinePoints = spline.getSplinePoints();
+
+        if (!splinePoints.isEmpty()) {
+            Point previousScreenSplinePoint = getPointOnScreen(splinePoints.get(0));
+            for (var point : splinePoints) {
+                Point screenPoint = getPointOnScreen(point);
+
+                g2d.drawLine(previousScreenSplinePoint.x, previousScreenSplinePoint.y, screenPoint.x, screenPoint.y);
+
+                previousScreenSplinePoint = screenPoint;
+            }
+        }
     }
 }
