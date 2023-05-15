173b7f6ef520d2e521115261fa3ecbf366ff41e5
shift-lab-kuznetsov <e.kuznetsov1@g.nsu.ru>
2023-05-15 23:31:01 +0700

    Закончил камеру

--- src/main/java/ru/nsu/fit/g20204/kuznetsov/wireframe/node/CameraNode.java
+++ src/main/java/ru/nsu/fit/g20204/kuznetsov/wireframe/node/CameraNode.java
@@ -1,5 +1,7 @@
 package ru.nsu.fit.g20204.kuznetsov.wireframe.node;
 
+import ru.nsu.fit.g20204.kuznetsov.wireframe.math.Matrix;
+
 public class CameraNode extends Node {
     private double cameraViewPortWidth = 4;
     private double cameraViewPortHeight = 2;
@@ -11,5 +13,38 @@ public class CameraNode extends Node {
         super(parentNode);
     }
 
+    public Matrix getViewportTransform() {
+        return new Matrix(new double[][] {
+                {nearClippingPlane, 0, 0, 0},
+                {0, nearClippingPlane, 0, 0},
+                {0, 0, farClippingPlane / (farClippingPlane - nearClippingPlane), farClippingPlane * nearClippingPlane / (farClippingPlane - nearClippingPlane)},
+                {0, 0, 1.0, 0.0}
+        });
+    }
 
+    public double getViewPortHeight() {
+        return cameraViewPortHeight;
+    }
+    public double getViewPortWidth() {
+        return cameraViewPortWidth;
+    }
+    public void setViewPortWidth(double width) {
+        this.cameraViewPortWidth = width;
+    }
+    public void setViewPortHeight(double height) {
+        this.cameraViewPortHeight = height;
+    }
+
+    public double getNearClippingPlane() {
+        return nearClippingPlane;
+    }
+    public void setNearClippingPlane(double nearClippingPlane) {
+        this.nearClippingPlane = nearClippingPlane;
+    }
+    public double getFarClippingPlane() {
+        return farClippingPlane;
+    }
+    public void setFarClippingPlane(double farClippingPlane) {
+        this.farClippingPlane = farClippingPlane;
+    }
 }
